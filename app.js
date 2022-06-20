d134fb9e697ea37bcc70c5f972efff25cf41bca2
FedeBac2010 <fedebacigalupi2010@outlook.com>
2022-06-19 22:55:18 -0300

    Se agrego funcionalidad al login y a home

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res)=>{
 res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+    })
+
 
 app.listen(3000, ()=>{
     console.log ('linstening on http://localhost:3000')
